308c15c15e1d6b41deaceb25cc46c95b7a1b3f3d
salvatoreaprile1926-afk <salvatoreaprile1926@gmail.com>
2026-06-12 13:17:31 +0200

    Update RuoloService.java

--- src/repository/RuoloService.java
+++ src/repository/RuoloService.java
@@ -1,27 +1,28 @@
 package repository;
 
 import java.util.ArrayList;
+
 import model.RuoloAziendale;
 
 public class RuoloService {
-
 	ArrayList<RuoloAziendale> ruoli = new ArrayList<>();
-	
-	public void inserisciDipendente(RuoloAziendale ruolo) {
+
+	public void inserisciRuolo(RuoloAziendale ruolo) {
 		ruoli.add(ruolo);
 	}
-	
+
+	//serve a restituire le persone salvate nella arraylist persone, diventa l'indirizzo di memoria dell'arraylist persone
 	public ArrayList<RuoloAziendale> leggiRuolo(){
 		return ruoli;
 	}
-	
+
 	public RuoloAziendale cercaPerId(int id) {
-		for( RuoloAziendale ruolo: ruoli) {
+		for (RuoloAziendale ruolo : ruoli) {
 			if(ruolo.getId()==id) {
 				return ruolo;
-				}
-			
 			}
+
+		}
 		return null;
 	}
 }
