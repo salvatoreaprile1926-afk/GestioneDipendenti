292ea5f5df87b9f00ae72ed49b9f92bed7c5e397
salvatoreaprile1926-afk <salvatoreaprile1926@gmail.com>
2026-06-12 13:12:43 +0200

    Update TitoloStudio.java

--- src/model/TitoloStudio.java
+++ src/model/TitoloStudio.java
@@ -1,31 +1,55 @@
 package model;
 
+import java.util.Objects;
+
 public class TitoloStudio {
 	
 	private int id;
 	private String titolo;
 	
-	 public int getId() {
-		 return id;
-	 }
-	 public void setId(int id) {
-		 this.id = id;
-	 }
-	 public String getTitolo() {
-		 return titolo;
-	 }
-	 public void setTitolo(String titolo) {
-		 this.titolo = titolo;
-	 }
-	 
-	 public TitoloStudio() {}
-	 
-	 public TitoloStudio(int id, String titolo) {
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitoloStudio() {
+		return titolo;
+	}
+	public void setTitoloStudio(String titolo) {
+		this.titolo = titolo;
+	}
+	
+	public TitoloStudio() {}
+	
+	public TitoloStudio(int id, String titolo) {
+		super();
 		this.id = id;
 		this.titolo = titolo;
-	 }
-	 public TitoloStudio(TitoloStudio titolo) {
-			this.id = titolo.id;
-			this.titolo = titolo.titolo;
-		 }
+	}
+
+	public TitoloStudio(TitoloStudio titolo) {
+		this.id = titolo.id;
+		this.titolo = titolo.titolo;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, titolo);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TitoloStudio other = (TitoloStudio) obj;
+		return id == other.id && Objects.equals(titolo, other.titolo);
+	}
+	
+	
+
+	    
+
 }
