257fec9338154a7b43630a0f7fe3b0a2baecacd4
salvatoreaprile1926-afk <salvatoreaprile1926@gmail.com>
2026-06-12 13:13:54 +0200

    Update VistaRuoli.java

--- src/view/VistaRuoli.java
+++ src/view/VistaRuoli.java
@@ -1,23 +1,50 @@
 package view;
 
+import java.util.ArrayList;
+
+
 import model.RuoloAziendale;
 
 public class VistaRuoli {
 	
-	VistaUtilita vistaUtilita= new VistaUtilita();
+	private VistaUtilita vistaUtilita;
+
 
 	public void menuRuoli() {
 		System.out.println("#### MENU RUOLI AZIENDALI ####");
-		System.out.println("1) Gestisci ");
+		System.out.println("1) Inserisci ");
 		System.out.println("2) Visualizza ");
 		System.out.println("3) Cerca ");
 		System.out.println("4) Ritorna al Menu Principale");
 		System.out.println("##############");
 		System.out.println();
 	}
-	
+
 	public void mascheraInserimento (RuoloAziendale ruolo) {
 		ruolo.setId(vistaUtilita.leggiIntero("ID: "));
 		ruolo.setRuoloAziendale(vistaUtilita.leggiStringa("Ruolo Aziendale: "));
-		}
 	}
+	
+	public void scheda (RuoloAziendale ruolo) {
+			System.out.println("\nID: " + ruolo.getId() +
+					"\nRuolo: " + ruolo.getRuoloAziendale());
+		}
+	
+	public void visualizza(ArrayList<RuoloAziendale> ruoli) {
+		if (ruoli.isEmpty()) {
+			System.out.println("Nessuna ruolo aziendale inserito.");
+		} else {
+			for (int i = 0; i < ruoli.size(); i++) {
+				scheda(ruoli.get(i));
+
+				System.out.println("------------------");
+			}
+		}
+	
+	}
+	
+	public VistaRuoli(VistaUtilita vistaUtilita) {
+		this.vistaUtilita=vistaUtilita;
+	}
+}
+
