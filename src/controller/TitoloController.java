2cc7559a4f0e4db7bd1bcf105b1cffc9db10b5ed
salvatoreaprile1926-afk <salvatoreaprile1926@gmail.com>
2026-06-12 13:06:56 +0200

    Update TitoloController.java

--- src/controller/TitoloController.java
+++ src/controller/TitoloController.java
@@ -1,22 +1,59 @@
 package controller;
 
-import repository.DipendenteService;
-import repository.RuoloService;
+
+import model.TitoloStudio;
 import repository.TitoloService;
-import view.VistaDipendenti;
 import view.VistaTitoli;
 import view.VistaUtilita;
 
-public class TitoloController {
-	
+public class TitoloStudioController {
+
 	private TitoloService titoloService;
-	private VistaTitoli vistaTitoli;
 	private VistaUtilita vistaUtilita;
-	
-	public TitoloController (TitoloService titoloService, VistaTitoli vistaTitoli, VistaUtilita vistaUtilita) {
-		
-	    this.titoloService = titoloService;
-	    this.vistaTitoli = vistaTitoli;
-	    this.vistaUtilita = vistaUtilita;
-}
+
+	public TitoloStudioController(TitoloService titoloService, VistaUtilita vistaUtilita) {
+		this.titoloService = titoloService;
+		this.vistaUtilita = vistaUtilita;
+	}
+
+	public void avvio() {
+		int scelta;
+		TitoloStudio titolo;
+		VistaTitoli vistaTitoli = new VistaTitoli(vistaUtilita);
+		do {
+			vistaTitoli.menuTitoli();
+			scelta=vistaUtilita.leggiIntero("Scegli: ");
+			switch(scelta) {
+			case 1:
+				titolo = new TitoloStudio();
+				vistaTitoli.mascheraInserimento(titolo);
+				titoloService.inserisciTitolo(titolo);
+				vistaUtilita.visualizzaMessaggio("Titolo inserito correttamente.");
+				vistaUtilita.leggiStringa("Premi un tasto per Continuare.");
+				break;
+			case 2:
+				vistaTitoli.visualizza(titoloService.leggiTitolo());
+				vistaUtilita.leggiStringa("Premi un tasto per Continuare.");
+				break;
+			case 3:
+				int id = vistaUtilita.leggiIntero("Inserisci ID da cercare: ");
+				TitoloStudio trovato = titoloService.cercaPerId(id);
+				if (trovato != null)
+					vistaTitoli.scheda(trovato);
+				else
+					vistaUtilita.visualizzaMessaggio("Titolo non trovato.");
+					vistaUtilita.leggiStringa("Premi un tasto per Continuare.");
+					break;
+			case 4:
+				vistaUtilita.visualizzaMessaggio("Ritorno al menu principale.");
+				break;	
+			default:
+				vistaUtilita.visualizzaMessaggio("Scelta non valida");
+
+			}
+
+		}
+		while(scelta != 4);
+	}
+
 }
