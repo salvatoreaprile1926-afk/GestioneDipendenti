1169fba48ed4038216143fffb248608377442e54
salvatoreaprile1926-afk <salvatoreaprile1926@gmail.com>
2026-06-12 13:15:50 +0200

    Update VistaTitoli.java

--- src/view/VistaTitoli.java
+++ src/view/VistaTitoli.java
@@ -1,24 +1,50 @@
 package view;
 
+import java.util.ArrayList;
+
+import model.RuoloAziendale;
 import model.TitoloStudio;
 
 public class VistaTitoli {
-	
-	VistaUtilita vistaUtilita= new VistaUtilita();
+
+	private VistaUtilita vistaUtilita;
 
 	public void menuTitoli() {
-		System.out.println("#### MENU RUOLI AZIENDALI ####");
-		System.out.println("1) Gestisci ");
+		System.out.println("#### MENU TITOLI DI STUDIO ####");
+		System.out.println("1) Inserisci ");
 		System.out.println("2) Visualizza ");
 		System.out.println("3) Cerca ");
 		System.out.println("4) Ritorna al Menu Principale");
 		System.out.println("##############");
 		System.out.println();
 	}
-	
+
 	public void mascheraInserimento (TitoloStudio titolo) {
 		titolo.setId(vistaUtilita.leggiIntero("ID: "));
-		titolo.setTitolo(vistaUtilita.leggiStringa("Titolo Studio: "));
+		titolo.setTitoloStudio(vistaUtilita.leggiStringa("Titolo di Studio: "));
+	}
+	
+	public void scheda (TitoloStudio titolo) {
+		System.out.println("\nID: " + titolo.getId() +
+				"\nRuolo: " + titolo.getTitoloStudio());
+	}
+	
+	public void visualizza(ArrayList<TitoloStudio> titoli) {
+		if (titoli.isEmpty()) {
+			System.out.println("Nessuna titolo di studio inserito.");
+		} else {
+			for (int i = 0; i < titoli.size(); i++) {
+				scheda(titoli.get(i));
+
+				System.out.println("------------------");
+			}
 		}
+	
+	}
+	
+	public VistaTitoli(VistaUtilita vistaUtilita) {
+		this.vistaUtilita=vistaUtilita;
+	}
+	
+
 }
- 
