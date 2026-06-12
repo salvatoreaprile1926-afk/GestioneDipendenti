28be3f07049d4ecbf8d34f999df9d64e878653d5
salvatoreaprile1926-afk <salvatoreaprile1926@gmail.com>
2026-06-12 13:06:25 +0200

    Update RuoloController.java

--- src/controller/RuoloController.java
+++ src/controller/RuoloController.java
@@ -1,21 +1,57 @@
 package controller;
 
+import model.RuoloAziendale;
 import repository.RuoloService;
-import repository.TitoloService;
 import view.VistaRuoli;
-import view.VistaTitoli;
 import view.VistaUtilita;
 
-public class RuoloController {
-	
+public class RuoloAziendaleController {
+
 	private RuoloService ruoloService;
-	private VistaRuoli vistaRuoli;
 	private VistaUtilita vistaUtilita;
-	
-	public RuoloController (RuoloService ruoloService, VistaRuoli vistaruoli, VistaUtilita vistaUtilita) {
-		
+
+	public RuoloAziendaleController(RuoloService ruoloService, VistaUtilita vistaUtilita) {
 		this.ruoloService = ruoloService;
-	    this.vistaRuoli = vistaRuoli;
-	    this.vistaUtilita = vistaUtilita;
+		this.vistaUtilita = vistaUtilita;
 	}
+
+	public void avvio () {
+		int scelta;
+		RuoloAziendale ruolo;
+		VistaRuoli vistaRuoli = new VistaRuoli(vistaUtilita);
+		do {
+			vistaRuoli.menuRuoli();;
+			scelta=vistaUtilita.leggiIntero("Scegli: ");
+			switch(scelta) {
+			case 1:
+				ruolo = new RuoloAziendale();
+				vistaRuoli.mascheraInserimento( ruolo);
+				ruoloService.inserisciRuolo(ruolo);
+				vistaUtilita.visualizzaMessaggio("Ruolo inserito correttamente.");
+				vistaUtilita.leggiStringa("Premi un tasto per Continuare.");
+				break;
+			case 2:
+				vistaRuoli.visualizza(ruoloService.leggiRuolo());
+				vistaUtilita.leggiStringa("Premi un tasto per Continuare.");
+				break;
+			case 3:
+				int id = vistaUtilita.leggiIntero("Inserisci ID da cercare: ");
+				RuoloAziendale trovato = ruoloService.cercaPerId(id);
+				if (trovato != null)
+					vistaRuoli.scheda(trovato);
+				else
+					vistaUtilita.visualizzaMessaggio("Ruolo non trovato.");
+					vistaUtilita.leggiStringa("Premi un tasto per Continuare.");
+					break;
+			case 4:
+				vistaUtilita.visualizzaMessaggio("Ritorno al menu principale.");
+				break;	
+			default:
+				vistaUtilita.visualizzaMessaggio("Scelta non valida");
+				
+			}
+		}
+		while(scelta != 4);
+	}
+
 }
